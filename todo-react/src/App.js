a7b6aae690ca536654ff768e66328817b8b5ef94
William Ryuputra <williamryuputra@yahoo.co.id>
2023-04-06 18:30:16 -0400

    Sets up layout for app

---
+++ todo-react/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="ToDoList">
+
+      <h1>To Do List</h1>
+      <div className="Tasks">
+        <h2>Tasks</h2>
+        {/* <NewTask />
+        <Task /> */}
+
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
